57465053f4dfe1a4072ea5dd81fae980aa79d580
Sanchit <46542791+SanchitVerma@users.noreply.github.com>
2020-02-03 14:55:42 -0600

    first draft - Zephyr analysis

--- Zephyr Analysis.R
+++ Zephyr Analysis.R
@@ -12,7 +12,7 @@
 
 # Import Zephyr's pull request data
 library(readxl)
-Zephyr_Data <- read_excel("Documents/GA Work/Zephyr Data.xlsx")
+Zephyr_Data <- read_excel("Documents/GA Work/Prof Matt/Zephyr Data.xlsx")
 View(Zephyr_Data) 
 
 # Names of allvariables in the dataset
@@ -30,11 +30,30 @@ str(Zephyr_Data)
 #$ lines_added    : num  16 18 676 2 12 32 92 8 136 0 ...
 #$ pr_src_state   : chr  "closed" "closed" "closed" "closed" ...
 
-# Converting categorial variable (y) to Binary
+# PR State distribution
+ggplot(Zephyr_Data, aes(pull_request_id, colour = pr_src_state)) + geom_histogram()
 
+# Converting character variable (y) to factor 
+Zephyr_Data <- as.data.frame(unclass(Zephyr_Data))
+str(Zephyr_Data)
 
+# Multiple Regression
+library(GGally)
+require(GGally)
 
-library(aod)
-library(ggplot2)
+# Correlation summary
+ggpairs(Zephyr_Data)
 
+# Multiple regression model (Non-logistic)
+#model <- lm((pr_src_state)~pull_request_id+pr_src_id+count_filenames+lines_added,data = Zephyr_Data)
+#summary(model)
 
+# Logistic Regression model - Attempt 1
+logistic_model <- glm(Zephyr_Data$pr_src_state ~ Zephyr_Data$pull_request_id + Zephyr_Data$pr_src_id + Zephyr_Data$count_filenames + Zephyr_Data$lines_added, family = binomial(link="logit"))
+summary(logistic_model)
+anova(logistic_model, test = 'Chisq')
+
+# Logistic Regression model - Attempt 2 without the ids
+logistic_model_2 <- glm(Zephyr_Data$pr_src_state ~ Zephyr_Data$count_filenames + Zephyr_Data$lines_added, family = binomial(link="logit"))
+summary(logistic_model_2)
+anova(logistic_model_2, test = 'Chisq')
